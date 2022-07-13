acf2412ccdc1766c1c02ae833d7adbfba26d8666
tonible14012002 <namanhble14012002@gmail.com>
2022-07-14 00:45:40 +0700

    feat header

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import {useEffect, useRef, useState} from 'react'
+
+import {Link} from 'react-router-dom'
+import styles from './Header.module.css'
+
+
+function Header(props){
+    return (
+        <div className={styles.header}>
+            <Link className={styles.header_logo} to='#'>
+                <img alt='logo' src='logo.png'></img>
+            </Link>
+            {!props.disable &&
+             <HeaderMenu/>                
+            }
+
+        </div>
+    )
+}
+
+function HeaderMenu(){
+
+    const options = useRef([
+        'Trang chủ',
+        'Phim T.Hình',
+        'Phim',
+        'Mới & Phổ biến',
+        'Danh sách của tôi'
+    ]).current
+    const [option, setOption] = useState(0)
+
+    useEffect(()=>{
+        
+    }, [option])
+
+    return (
+        <ul className={styles.header_menu}>
+            {options.map((op, index) => {
+                return (
+                    <li
+                        key={index}
+                        className={styles.menu_option}
+                        onClick={()=>{
+                            if (index != option){
+                                setOption(index)
+                            }
+                        }}
+                        style={
+                            option==index ? {
+                                color:'#ffff',
+                                cursor: 'default'
+                            }:{}
+                        }
+                    >
+                       {op} 
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default Header
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,12 @@
 import styles from './Login.module.css'
 import {Link} from 'react-router-dom'
-import Navbar from './Navbar'
+import Header from './Header'
 import { useLayoutEffect, useState } from 'react'
 
 function LoginPage(props){
     return (
         <>
-        <Navbar/>
+        <Header />
         <Login/>
         </>
     )
@@ -16,18 +16,37 @@ function Login(props) {
     const [profiles, setProfiles] = useState([])
     useLayoutEffect(()=>{
         // fake fetch api
-        
+        setProfiles([
+            {
+                id: 1,
+                name: 'Nam Anh',
+                iconID: 2
+            },
+            {
+                id: 2,
+                name: 'Nhã',
+                iconID: 4
+            },            {
+                id: 3,
+                name: 'Thien Kim',
+                iconID: 1
+            },            {
+                id: 4,
+                name: 'Mai Thy',
+                iconID: 3
+            },            {
+                id: 5,
+                name: 'Children',
+                iconID: 2
+            },
+        ])
     },[])
     return (
         <div className={styles.profile_gate_wrapper}>
             <div className={styles.profile_gate}>
                 <h1 className={styles.profile_gate_title}>Who is watching ?</h1>
                 <div className={styles.profile_list}>
-                    <ProfileIcon>Nam Anh</ProfileIcon>
-                    <ProfileIcon>Thien Kim</ProfileIcon>
-                    <ProfileIcon>Nam Anh</ProfileIcon>
-                    <ProfileIcon>Nam Anh</ProfileIcon>
-                    <ProfileIcon>Nam Anh</ProfileIcon>
+                    {profiles.map((profile) => <ProfileIcon key={profile.id} children={profile.name}/>)}
                 </div>              
                 <ManagerButton>Profile manager</ManagerButton>
             </div>
@@ -39,7 +58,7 @@ function ProfileIcon({children, ...props}) {
     return (
         <div className= {styles.profile_wrapper}>
             <Link className={styles.profile_link} to='/login'>
-                <img className={styles.profile__icon} title={children} src='https://scontent.fsgn8-2.fna.fbcdn.net/v/t39.30808-1/275937932_3223390564592461_8256715824947888335_n.jpg?stp=dst-jpg_p240x240&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_ohc=qS0hkKLlCNkAX88bjFB&_nc_ht=scontent.fsgn8-2.fna&oh=00_AT_tY3Xque7obtohxftiWZxeMF9rWO0EKrrDVSkAXlZdAA&oe=62D40FD0' />
+                <img alt={children} className={styles.profile__icon} title={children} src='https://scontent.fsgn8-2.fna.fbcdn.net/v/t39.30808-1/275937932_3223390564592461_8256715824947888335_n.jpg?stp=dst-jpg_p240x240&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_ohc=qS0hkKLlCNkAX88bjFB&_nc_ht=scontent.fsgn8-2.fna&oh=00_AT_tY3Xque7obtohxftiWZxeMF9rWO0EKrrDVSkAXlZdAA&oe=62D40FD0' />
                 <span className={styles.profile__name}>{children}</span>
             </Link>
         </div>
